6734415236d3747baead955e5011a75660192316
Kamil.Zabinski <Kamil.Zabinski@upc.pl>
2020-04-18 18:41:00 +0200

    Add_monads

--- monads/src/main/java/pl/writeonly/java/monads/Util.java
+++ monads/src/main/java/pl/writeonly/java/monads/Util.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Util {
   public static void main(String[] args) {
-    final var nestedMap = Map.of("key1", Map.of("key2", Map.of("key3", "someValue")));
+    final var nestedMap = Map.of("key1", Map.of("key2", Map.of("key3", "MonadsAreAwesome")));
 
     System.out.println(Optional.ofNullable(nestedMap.get("key1")).flatMap(v1 -> Optional.ofNullable(v1.get("key2")).flatMap(v2 -> Optional.ofNullable(v2.get("key3")))));
     System.out.println(Optional.ofNullable(nestedMap.get("key1")).flatMap(v1 -> Optional.ofNullable(v1.get(":()")).flatMap(v2 -> Optional.ofNullable(v2.get("key3")))));
--- monads/src/main/java/pl/writeonly/java/monads/Vavr.java
+++ monads/src/main/java/pl/writeonly/java/monads/Vavr.java
@@ -4,7 +4,7 @@ import io.vavr.collection.*;
 
 public class Vavr {
   public static void main(String[] args) {
-    final var nestedMap = HashMap.of("key1", HashMap.of("key2", HashMap.of("key3", "someValue")));
+    final var nestedMap = HashMap.of("key1", HashMap.of("key2", HashMap.of("key3", "MonadsAreAwesome")));
 
     System.out.println(nestedMap.get("key1").flatMap(v1 -> v1.get("key2")).flatMap(v2 -> v2.get("key3")));
     System.out.println(nestedMap.get("key1").flatMap(v1 -> v1.get(":()")).flatMap(v2 -> v2.get("key3")));
