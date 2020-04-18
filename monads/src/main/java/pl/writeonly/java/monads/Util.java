d163b06ed7efd268a6654effd1706028f8234c06
Kamil.Zabinski <Kamil.Zabinski@upc.pl>
2020-04-18 18:24:04 +0200

    Add_monads

--- monads/src/main/java/pl/writeonly/java/monads/Util.java
+++ monads/src/main/java/pl/writeonly/java/monads/Util.java
@@ -3,7 +3,7 @@ package pl.writeonly.java.monads;
 import java.util.*;
 
 public class Util {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     final var nestedMap = Map.of("key1", Map.of("key2", Map.of("key3", "someValue")));
 
     System.out.println(Optional.ofNullable(nestedMap.get("key1")).flatMap(v1 -> Optional.ofNullable(v1.get("key2")).flatMap(v2 -> Optional.ofNullable(v2.get("key3")))));
